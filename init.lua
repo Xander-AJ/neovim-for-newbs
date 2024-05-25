29286245ad66cbdf9b5f531a3febe36aab2a453a
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-05-25 16:08:19 +0300

    sets require and adds path to custom_keybindings.lua

--- init.lua
+++ init.lua
@@ -9,20 +9,8 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-
-local vim = require('vim')
-
--- Map copy, paste, and cut to your preferred keybindings
-vim.keymap.set('n', '<C-c>', '"+y') -- Copy to clipboard register (+)
-vim.keymap.set('n', '<C-v>', 'p')   -- Paste from clipboard
-vim.keymap.set('n', '<C-x>', '"+d') -- Cut to clipboard register (+)
-
--- Alternatively, for visual mode:
--- vim.keymap.set('v', '<C-c>', '"+y') 
--- vim.keymap.set('v', '<C-v>', 'p') 
--- vim.keymap.set('v', '<C-x>', '"+d') 
-
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
+require("custom_keybindings")
