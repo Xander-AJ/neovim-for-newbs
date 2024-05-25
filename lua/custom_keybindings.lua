4e3cdf8e10499fd2ae9a853fac2765b4db061cb0
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-05-25 16:21:53 +0300

    sets custom shortcuts for copy, paste and cut

--- lua/custom_keybindings.lua
+++ lua/custom_keybindings.lua
@@ -1,7 +1,9 @@
 -- Map copy, paste, and cut to your preferred keybindings
-vim.keymap.set('n', '<C-c>', '"+y') -- Copy to clipboard register (+)
-vim.keymap.set('n', '<C-v>', 'p') -- Paste from clipboard
-vim.keymap.set('n', '<C-x>', '"+d') -- Cut to clipboard register (+)
+
+vim.keymap.set('n', '<C-S-c>', '"+y') -- Copy to clipboard register (+)
+vim.keymap.set('n', '<C-S-v>', 'p') -- Paste from clipboard
+vim.keymap.set('n', '<C-S-x>', '"+d') -- Cut to clipboard register (+)
+
 
 -- Alternatively, for visual mode:
 -- vim.keymap.set('v', '<C-c>', '"+y') 
